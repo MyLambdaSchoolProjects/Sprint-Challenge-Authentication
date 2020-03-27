78dd58b5d5d0b6d6228089654802c3eaddcea156
David Isakson <ikeman32@yahoo.com>
2020-03-27 08:37:36 -0700

    server set up

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require(`morgan`);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -9,10 +11,15 @@ const jokesRouter = require('../jokes/jokes-router.js');
 const server = express();
 
 server.use(helmet());
+server.use(morgan("dev"));
 server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up', dbenv: process.env.DB_ENV });
+});
 
 module.exports = server;
